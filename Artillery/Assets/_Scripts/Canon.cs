1a495fd106b74240d343ea29dc9c801168a4cb0b
Sajhir98 <allanuc1428@gmail.com>
2026-08-08 12:26:27 -0500

    Modulo 17
    Se realizan las tareas asignadas
    - se crea un efecto de explosión usando assets de opengamerart
    -se crea un efecto para la bala usando Shader Graph,
    -se crea el efecto de disparo para el cañon

--- Artillery/Assets/_Scripts/Canon.cs
+++ Artillery/Assets/_Scripts/Canon.cs
@@ -1,8 +1,16 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Canon : MonoBehaviour
 {
+    public static bool Bloqueado;
+
+    public AudioClip clipDisparo;
+    private GameObject SonidoDisparo;
+    private AudioSource SourceDisparo; 
+
     [SerializeField] private GameObject BalaPrefab;
+    public GameObject ParticulasDisparo;
     private GameObject PuntaCanon;
     private float rotacion;
 
@@ -10,6 +18,8 @@ public class Canon : MonoBehaviour
     private void Start()
     {
         PuntaCanon = transform.Find("Cañon/PuntaCanon").gameObject;
+        SonidoDisparo = GameObject.Find("SonidoDisparo");
+        SourceDisparo = SonidoDisparo.GetOrAddComponent<AudioSource>();
     }
 
     
@@ -24,19 +34,24 @@ public class Canon : MonoBehaviour
         if (rotacion > 90) rotacion = 90;
         if (rotacion < 0) rotacion = 0;
 
-        if (Input.GetKeyDown(KeyCode.Space) && AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego > 0)
+        if (Input.GetKeyDown(KeyCode.Space) && AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego > 0 && !Bloqueado)
         {
             AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego--;
 
             GameObject temp = Instantiate(BalaPrefab, PuntaCanon.transform.position, transform.rotation);
             Rigidbody tempRB = temp.GetComponent<Rigidbody>();
+            SeguirCamara.objetivo = temp;
 
             Vector3 direccionDisparo = transform.rotation.eulerAngles;
             direccionDisparo.y = 90 - direccionDisparo.x;
-
+            Vector3 direcciónparticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
+            GameObject Particulas = Instantiate
+                (ParticulasDisparo, PuntaCanon.transform.position, Quaternion.Euler(direcciónparticulas),transform);
             tempRB.linearVelocity = direccionDisparo.normalized * AdministradorJuego.SingletonAdministradorJuego.VelocidadBala;
-
+            //SourceDisparo.PlayOneShot(clipDisparo);
+            SourceDisparo.Play();
             Debug.Log($"Disparos restantes: {AdministradorJuego.SingletonAdministradorJuego.DisparosPorJuego}");
+            Bloqueado = true;
         }
     }
 }
